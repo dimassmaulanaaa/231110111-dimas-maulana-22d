1f143702de15aebefaf3c3cea678806cd27d1988
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-05-09 22:45:10 +0700

    Initial commit

---
+++ helpers/alertHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+function alertsForCRUD($conn, $query, $message, $url)
+{
+    if (mysqli_query($conn, $query)) {
+        $_SESSION['notification'] = [
+            'type' => [
+                'success',
+                'success'
+            ],
+            'message' => $message
+        ];
+        header("Location: $url");
+        exit();
+    } else {
+        $_SESSION['notification'] = [
+            'type' => [
+                'danger',
+                'warning'
+            ],
+            'message' => $message
+        ];
+    }
+}
+
+
+function successAlerts($message)
+{
+    $_SESSION['notification'] = [
+        'type' => [
+            'success',
+            'success'
+        ],
+        'message' => $message
+    ];
+}
+
+
+function warningAlerts($message)
+{
+    $_SESSION['notification'] = [
+        'type' => [
+            'danger',
+            'warning'
+        ],
+        'message' => $message
+    ];
+}
